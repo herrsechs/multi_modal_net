bc7b9d179e63ff8193663c5bcd855e1395be60ca
liuyajun <herrsechs@foxmail.com>
2019-01-16 11:47:32 +0800

    Add experiment.

--- experiments/yolo_motion.py
+++ experiments/yolo_motion.py
@@ -1,13 +1,2 @@
-from prediction.predict import get_prediction
-from training.train import get_classifier
+from training.train import train_model, eval_model, get_classifier
 from data_IO.data_reader import get_data
-from statistic.calculation import confusion_matrix
-
-LABEL_PATH = './label.txt'
-DATA_PATH = './data'
-MODEL_PATH = './model'
-
-labels, imgs, _ = get_data(DATA_PATH, LABEL_PATH)
-clf = get_classifier(MODEL_PATH, 'alex_net')
-pred = get_prediction(clf, {'X': imgs}, labels)
-confusion_matrix(pred, labels, show_mat=True)
