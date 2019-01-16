9a61a9301317052ee1a6dfc20fb98bc2ff17fed9
liuyajun <herrsechs@foxmail.com>
2019-01-16 14:41:54 +0800

    1. Close tensorflow log info. 2. Add sys path.

--- experiments/yolo_motion.py
+++ experiments/yolo_motion.py
@@ -1,13 +1,18 @@
+import sys
+import os
+sys.path.append('../')
 from prediction.predict import get_prediction
 from training.train import get_classifier
 from data_IO.data_reader import get_data
 from statistic.calculation import confusion_matrix
 
-LABEL_PATH = './label.txt'
-DATA_PATH = './data'
-MODEL_PATH = './model'
+LABEL_PATH = '/home/liuyajun/concat_net/data/kfold_label/test_k0_label.csv'
+DATA_PATH = '/home/liuyajun/concat_net/data/yolo_motion/'
+MODEL_PATH = '/home/liuyajun/concat_net/model/yolo_motion/k0/'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 labels, imgs, _ = get_data(DATA_PATH, LABEL_PATH)
 clf = get_classifier(MODEL_PATH, 'alex_net')
-pred = get_prediction(clf, {'X': imgs}, labels)
+pred = get_prediction(clf, {'X1': imgs}, labels)
 confusion_matrix(pred, labels, show_mat=True)
